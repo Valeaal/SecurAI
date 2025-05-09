b69f5ca5ca8577890f450676b36ebc55f4d24951
Valenciaal <valeal@uma.es>
2025-05-09 20:13:04 +0200

    werkzeug no me va a impedir seguir ahora

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -1,9 +1,7 @@
 from app import socketio
 from app import createApp
 
-
-
 app = createApp()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=4000)
+    socketio.run(app, host='0.0.0.0', port=4000, allow_unsafe_werkzeug=True)
